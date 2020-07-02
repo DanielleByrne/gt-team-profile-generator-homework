2d0587344ef7382c8389a591e9b9e7c34d258558
Danielle Byrne <DanielleByrne421@gmail.com>
2020-07-02 17:39:28 -0400

    beginning of classes for each role

--- develop/lib/Engineer.js
+++ develop/lib/Engineer.js
@@ -1,1 +1,20 @@
 // TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+class Engineer extends Employee {
+    constructor(name, id, email) {
+      this.name = name;
+      this.id = id;
+      this.email = email;
+    }
+    getName() {
+        console.log(this.name);
+    }
+    getId(){
+        console.log(this.id);
+    }
+    getEmail(){
+        console.log(this.email)
+    }
+    getRole(role){
+       
+    }
+  }
